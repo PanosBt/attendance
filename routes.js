d21fc84f96191f8aa6f5dd479b02f579ab348a97
PanosBt <panbout@hotmail.gr>
2024-02-12 17:59:33 +0000

    auth routes

--- routes.js
+++ routes.js
@@ -1,4 +1,5 @@
 import koaRouter from 'koa-router';
+
 import * as IndexController from './controllers/index.js';
 import * as DeclareAttendanceController from './controllers/declare_attendance.js';
 import * as RoomsController from './controllers/rooms.js';
@@ -7,14 +8,21 @@ import * as LogoutController from './controllers/logout.js';
 
 const router = new koaRouter();
 
-router.get('/', IndexController.get);
-// TODO Check DRY if authenticated + on the right role as middleware
-router.get('/declare_attendance', DeclareAttendanceController.get);
-router.get('/rooms', RoomsController.getRooms);
+const auth_middleware = async (ctx, next) => {
+    if (!ctx.isAuthenticated()) {
+        ctx.redirect('/');
+    }
+    await next();
+};
 
-router.get('/edit_room', RoomsController.editRoomPage);
-router.post('/edit_room', RoomsController.editRoom);
-router.post('/delete_room', RoomsController.deleteRoom);
+router.get('/', IndexController.get);
+
+router.get('/declare_attendance', auth_middleware, DeclareAttendanceController.get);
+
+router.get('/rooms', auth_middleware, RoomsController.getRooms);
+router.get('/edit_room', auth_middleware, RoomsController.editRoomPage);
+router.post('/edit_room', auth_middleware, RoomsController.editRoom);
+router.post('/delete_room', auth_middleware, RoomsController.deleteRoom);
 
 router.post('/login', LoginController.login);
 router.post('/logout', LogoutController.logout);
